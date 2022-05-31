1d7bdb49c9a9603b6c92d7e99876c252e19e9036
Ricardo <limaricardop@gmail.com>
2022-05-31 20:10:08 -0300

    React connection with axios create

--- src/api/lucroRural.js
+++ src/api/lucroRural.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://lucrorural-dev-backend.herokuapp.com/'
+    baseURL: 'https://lucrorural-dev-backend.herokuapp.com'
 })
