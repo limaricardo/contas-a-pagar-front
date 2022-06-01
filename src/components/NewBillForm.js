798c016e84372135aa6fab393b52351f2d8f1db4
Ricardo <limaricardop@gmail.com>
2022-05-31 23:16:06 -0300

    Fix 'fornecedor'

--- src/components/NewBillForm.js
+++ src/components/NewBillForm.js
@@ -18,7 +18,7 @@ import { Button } from "@mui/material";
 import { toast } from "react-toastify";
 
 const NewBillForm = ({ setShowModal }) => {
-  const [fornecedor, setFornecedor] = useState("Maria");
+  const [fornecedor, setFornecedor] = useState(fornecedores[0]);
   const [fornecedores, setFornecedores] = useState([]);
 
   const [notaFiscal, setNotaFiscal] = useState([]);
